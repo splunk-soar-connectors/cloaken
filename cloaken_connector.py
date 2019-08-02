4b9dda7995e0baf86a7ebffd91ae84bd49dd27e8
Jay Patel <jay.patel@M06-JayP.local>
2019-08-02 18:54:13 +0530

    Cloaken: All bugs fixation for the app

--- cloaken_connector.py
+++ cloaken_connector.py
@@ -7,9 +7,6 @@ import phantom.app as phantom
 from phantom.base_connector import BaseConnector
 from phantom.action_result import ActionResult
 
-# Usage of the consts file is recommended
-# from cloaken_consts import *
-# import requests
 import json
 from bs4 import BeautifulSoup
 from cloakensdk.client import SyncClient
@@ -25,14 +22,9 @@ class CloakenConnector(BaseConnector):
 
     def __init__(self):
 
-        # Call the BaseConnectors init first
         super(CloakenConnector, self).__init__()
 
         self._state = None
-
-        # Variable to hold a base_url in case the app makes REST calls
-        # Do note that the app json defines the asset config, so please
-        # modify this as you deem fit.
         self._base_url = None
 
     def _process_empty_response(self, response, action_result):
@@ -40,7 +32,7 @@ class CloakenConnector(BaseConnector):
         if response.status_code == 200:
             return RetVal(phantom.APP_SUCCESS, {})
 
-        return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header"), None)
+        return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header."), None)
 
     def _process_html_response(self, response, action_result):
 
@@ -70,14 +62,14 @@ class CloakenConnector(BaseConnector):
             resp_json = r.json()
         except Exception as e:
             return RetVal(action_result.set_status(phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))), None)
+
         # do not stop on 400 error; that is a malformed url or dead link
         if r.status_code == 400:
             return RetVal(phantom.APP_SUCCESS, resp_json)
-        # Please specify the status codes here
+
         if 200 <= r.status_code < 399:
             return RetVal(phantom.APP_SUCCESS, resp_json)
 
-        # You should process the error returned in the json
         message = "Error from server. Status Code: {0} Data from server: {1}".format(
                 r.status_code, r.text.replace(u'{', '{{').replace(u'}', '}}'))
 
@@ -86,17 +78,13 @@ class CloakenConnector(BaseConnector):
 
     def _process_response(self, r, action_result):
 
-        # store the r_text in debug data, it will get dumped in the logs if the action fails
         if hasattr(action_result, 'add_debug_data'):
             action_result.add_debug_data({'r_status_code': r.status_code})
             action_result.add_debug_data({'r_text': r.text})
             action_result.add_debug_data({'r_headers': r.headers})
 
-        # Process each 'Content-Type' of response separately
-
         # Process a json response
         if 'json' in r.headers.get('Content-Type', ''):
-            self.save_progress("json")
             return self._process_json_response(r, action_result)
 
         # Process an HTML response, Do this no matter what the api talks.
@@ -104,12 +92,10 @@ class CloakenConnector(BaseConnector):
         # world, in case of errors, PROXY's return HTML, this function parses
         # the error and adds it to the action_result.
         if 'html' in r.headers.get('Content-Type', ''):
-            self.save_progress("html")
             return self._process_html_response(r, action_result)
 
         # it's not content-type that is to be parsed, handle an empty response
         if not r.text:
-            self.save_progress("empty")
             return self._process_empty_response(r, action_result)
 
         # everything else is actually an error at this point
@@ -118,32 +104,10 @@ class CloakenConnector(BaseConnector):
         self.save_progress("error:" + message)
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
-    def _make_rest_call(self, resource, action_result):
-        """
-        :param resource cloakensdk.Resource
-        """
-
-        # config = self.get_config()
-
-        resp_json = None
-        try:
-            self.save_progress("make call")
-            r = resource.request()
-            self.save_progress("response: " + str(r))
-        except Exception as e:
-            return RetVal(action_result.set_status( phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(str(e))), resp_json)
-
-        return self._process_response(r, action_result)
-
     def _handle_test_connectivity(self, param):
 
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
-
-        # NOTE: test connectivity does _NOT_ take any parameters
-        # i.e. the param dictionary passed to this handler will be empty.
-        # Also typically it does not add any data into an action_result either.
-        # The status and progress messages are more important.
 
         self.save_progress("Connecting to endpoint")
         ret_val, data = self._process_response(self.client.login_response, action_result)
@@ -152,32 +116,19 @@ class CloakenConnector(BaseConnector):
             action_result.set_status(phantom.APP_ERROR)
             # the call to the 3rd party device or service failed, action result should contain all the error details
             # for now the return is commented out, but after implementation, return from here
-            self.save_progress("Test Connectivity Failed." + str(ret_val))
+            self.save_progress("Test Connectivity Failed. " + str(ret_val))
             return action_result.get_status()
 
-        # Return success
         self.save_progress("Test Connectivity Passed")
         return action_result.set_status(phantom.APP_SUCCESS)
 
-        # For now return Error with a message, in case of success we don't set the message, but use the summary
-        # return action_result.set_status(phantom.APP_ERROR, "Action not yet implemented")
-
     def _handle_lookup_url(self, param):
 
-        # Implement the handler here
-        # use self.save_progress(...) to send progress messages back to the platform
         self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
 
-        # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
-        # Access action parameters passed in the 'param' dictionary
-
-        # Required values can be accessed directly
         url = param['url']
-
-        # Optional values should use the .get() function
-        # optional_parameter = param.get('optional_parameter', 'default_value')
 
         # make rest call
         resource = Url(self.client)
@@ -188,39 +139,28 @@ class CloakenConnector(BaseConnector):
         ret_val, data = self._process_response(response, action_result)
 
         self.save_progress("request processed: {0}".format(self.get_action_identifier()))
+
         if (phantom.is_fail(ret_val)):
-            # 404 errors or other errors
             return action_result.get_status()
         else:
-            # valid result
             action_result.add_data(data)
-
-        # Now post process the data,  uncomment code as you deem fit
-
-        # Add the response into the data section
 
         self.save_progress("data {0}".format(str(data)))
         if response.status_code == 400:
             action_result.update_summary({"original_url": url,
                 "error": data["url"]})
         else:
-            # Add a dictionary that is made up of the most important values from data into the summary
             action_result.update_summary({"original_url": url,
                 "unshortened_url": data["unshortened_url"]})
-        # summary['num_data'] = len(action_result['data'])
-        # Return success, no need to set the message, only the status
-        # BaseConnector will create a textual message based off of the summary dictionary
-        return action_result.set_status(phantom.APP_SUCCESS)
 
-        # For now return Error with a message, in case of success we don't set the message, but use the summary
-        # return action_result.set_status(phantom.APP_ERROR, "Action not yet implemented")
+        return action_result.set_status(phantom.APP_SUCCESS)
 
     def handle_action(self, param):
 
         ret_val = phantom.APP_SUCCESS
 
         self.save_progress("In handle action for: {0}".format(self.get_action_identifier()))
-        # Get the action that we are supposed to execute for this App Run
+
         action_id = self.get_action_identifier()
 
         self.debug_print("action_id", self.get_action_identifier())
@@ -235,32 +175,27 @@ class CloakenConnector(BaseConnector):
 
     def initialize(self):
 
-        # Load the state in initialize, use it to store data
-        # that needs to be accessed across actions
         self._state = self.load_state()
 
-        # get the asset config
         config = self.get_config()
 
-        # Access values in asset config by the name
-
         self.save_progress("Ininitialize: ")
-        # Required values can be accessed directly
         self._base_url = config.get('server_url')
         self.username = config["username"]
         self.password = config["password"]
-        self.client = SyncClient(server_url=self._base_url, username=self.username,
-                password=self.password)
-
-        self.save_progress("Ininitialize: ")
-        # Optional values should use the .get() function
-        # optional_config_name = config.get('optional_config_name')
+        try:
+            self.client = SyncClient(server_url=self._base_url, username=self.username,
+                    password=self.password)
+        except Exception as e:
+            try:
+                return self.set_status(phantom.APP_ERROR, str(e).encode('utf-8'))
+            except:
+                return self.set_status(phantom.APP_ERROR, "Error occurred while creating the client. Please verify the provided asset configuration parameters.")
 
         return phantom.APP_SUCCESS
 
     def finalize(self):
 
-        # Save the state, this data is saved across actions and app upgrades
         self.save_state(self._state)
         return phantom.APP_SUCCESS
 
@@ -286,7 +221,6 @@ if __name__ == '__main__':
 
     if (username is not None and password is None):
 
-        # User specified a username but not a password, so ask
         import getpass
         password = getpass.getpass("Password: ")
 
